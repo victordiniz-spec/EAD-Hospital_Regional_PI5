7911bf61d6caaa739e52b3655e983a373a04a135
Victor Nascimento <victordiniz@unipam.edu>
2026-05-02 12:50:48 -0300

    corrige layout app removendo navbar global antiga

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,28 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('title', 'Integrar ReSaúde')</title>
 
     @vite('resources/css/app.css')
 </head>
 
-<body class="text-gray-800">
+<body class="text-gray-800 bg-[#F3F7F3] min-h-screen">
 
-{{-- NAVBAR --}}
-@if(!isset($noLayout))
-    @include('components.navbar')
-@endif
+    {{-- 
+        NAVBAR GLOBAL DESATIVADA
+        Motivo: algumas telas, como dashboard do professor/aluno, já possuem navbar/sidebar própria.
+        Se quiser usar a navbar em uma tela específica, chame diretamente:
+        @include('partials.navbar')
+    --}}
 
-<div class="w-full min-h-screen flex items-center justify-center">
+    <div class="w-full min-h-screen">
 
-    @yield('content')
+        @yield('content')
 
-</div>
+    </div>
 
-{{-- FOOTER --}}
-@if(!isset($noLayout))
-    @include('components.footer')
-@endif
+    {{-- 
+        FOOTER GLOBAL OPCIONAL
+        Mantido apenas se o arquivo existir e se a tela não desativar layout.
+    --}}
+    @if(!isset($noLayout) && View::exists('components.footer'))
+        @include('components.footer')
+    @endif
 
 </body>
 </html>
